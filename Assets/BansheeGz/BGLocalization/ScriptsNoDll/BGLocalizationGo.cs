18e781b5e21194627464eb4abd404e3bf7525149
DESKTOP-RVQKH4O\yhyang-pc <yhyang@cookapps.com>
2022-01-07 19:04:19 +0900

    BGDB, 구글애드몹 추가

---
+++ Assets/BansheeGz/BGLocalization/ScriptsNoDll/BGLocalizationGo.cs
@@ -0,0 +1,15 @@
+/*
+<copyright file="BGLocalizationGo.cs" company="BansheeGz">
+    Copyright (c) 2018-2021 All Rights Reserved
+</copyright>
+*/
+
+using UnityEngine;
+
+namespace BansheeGz.BGDatabase
+{
+    [AddComponentMenu("BansheeGz/BGLocalizationGo")]
+    public partial class BGLocalizationGo : BGLocalizationGoA
+    {
+    }
+}
