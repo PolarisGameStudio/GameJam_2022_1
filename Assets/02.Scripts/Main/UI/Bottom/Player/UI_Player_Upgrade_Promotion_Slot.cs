a074c66f6c2ec15f11be5915efc366996847da1d
yhyang_Home <yeonghun23@gmail.com>
2022-01-18 03:15:04 +0900

    승급 UI 적용

--- Assets/02.Scripts/Main/UI/Bottom/Player/UI_Player_Upgrade_Promotion_Slot.cs
+++ Assets/02.Scripts/Main/UI/Bottom/Player/UI_Player_Upgrade_Promotion_Slot.cs
@@ -1,18 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
+using EnhancedScrollerDemos.Chat;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class UI_Player_Upgrade_Promotion_Slot : MonoBehaviour
+public class UI_Player_Upgrade_Promotion_Slot : UI_BaseSlot<TBL_PROMOTION>,GameEventListener<RefreshEvent>
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Image _imgPromotionIcon;
+
+    [SerializeField] private Text _txtPromotionName;
+    [SerializeField] private Text _txtPromotionStatValue;
+    [SerializeField] private Text _txtRecommendLevel;
+
+    [SerializeField]  private Button _btnChallange;
+    [SerializeField] private GameObject _onDisableObject;
+    
+
+    private void OnEnable()
     {
-        
+        this.AddGameEventListening<RefreshEvent>();
+
+        if (_data != null)
+        {
+            Refresh();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
+        this.RemoveGameEventListening<RefreshEvent>();
+    }
+    
+    public override void Init(TBL_PROMOTION data)
+    {
+        _data = data;
         
+        _imgPromotionIcon.sprite = null;
+        
+        _txtPromotionName.text = $"{_data.name}";
+
+        _txtPromotionStatValue.text =
+            $"공격력 x{_data.DamageMultipleValue} , 체력 x{_data.HealthMultipleValue}";
+
+        _txtRecommendLevel.text = "추천레벨 ?";
+        
+        Refresh();
+    }
+
+    private void Refresh()
+    {
+        _onDisableObject.gameObject.SetActive(!DataManager.PromotionData.IsAlreadyClear(_data.Index));
+
+        _btnChallange.interactable = DataManager.PromotionData.IsEnableChallenge(_data.Index);
+    }
+
+    public void OnGameEvent(RefreshEvent e)
+    {
+        if (e.Type == Enum_RefreshEventType.Battle)
+        {
+            Refresh();
+        }
+    }
+
+    public void OnClickChallengeButton()
+    {
+        DataManager.PromotionData.TryChallenge(_data.Index);
     }
 }
--- Assets/02.Scripts/Shadow/SaveData/PromotionData.cs
+++ Assets/02.Scripts/Shadow/SaveData/PromotionData.cs
@@ -1,38 +1,36 @@
 using System;
+using UnityEngine;
 
 public class PromotionData : StatData
 {
-    public int CurrenctPromotionIndex { get; set; }
+    [SerializeField] private int _currentPromotionIndex;
 
     [NonSerialized] public bool IsMaxPromotion;
-
-    public PromotionData()
-    {
-        CurrenctPromotionIndex = -1;
-    }
 
     public override void ValidCheck()
     {
         base.ValidCheck();
 
         CalculateStat();
+
+        _currentPromotionIndex = Mathf.Max(0, _currentPromotionIndex);
     }
 
     public void OnClearPromotionBattle()
     {
-        CurrenctPromotionIndex++;
-        
+        _currentPromotionIndex++;
+
         CalculateStat();
     }
-    
+
     protected override void CalculateStat()
     {
         Stat.Init();
 
         Stat[Enum_StatType.Damage] = 1;
         Stat[Enum_StatType.MaxHealth] = 1;
-        
-        for (int i = 0; i <= CurrenctPromotionIndex; i++)
+
+        for (int i = 0; i <= _currentPromotionIndex; i++)
         {
             if (i >= TBL_PROMOTION.CountEntities)
             {
@@ -44,14 +42,37 @@ public class PromotionData : StatData
             if (data.DamageMultipleValue >= 1)
             {
                 Stat[Enum_StatType.Damage] *= data.DamageMultipleValue;
-            } 
-            
+            }
+
             if (data.HealthMultipleValue >= 1)
-            {            
+            {
                 Stat[Enum_StatType.MaxHealth] *= data.HealthMultipleValue;
             }
         }
-        
+
         RefreshEvent.Trigger(Enum_RefreshEventType.StatChange);
+    }
+
+    public bool IsEnableChallenge(int index)
+    {
+        return _currentPromotionIndex + 1 == index;
+    }
+
+    public bool IsAlreadyClear(int index)
+    {
+        return _currentPromotionIndex >= index;
+    }
+
+
+    public bool TryChallenge(int index)
+    {
+        if (!IsEnableChallenge(index))
+        {
+            return false;
+        }
+        
+        BattleManager.Instance.BattleStart(Enum_BattleType.PromotionBattle, index);
+
+        return true;
     }
 }
